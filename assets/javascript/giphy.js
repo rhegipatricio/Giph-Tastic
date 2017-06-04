f5d22a67518ae51f99fd0d8c2dc41d0c7e8c29a1
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-04 15:01:13 -0400

    added on click event

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -17,14 +17,58 @@ var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orla
         	}
         }
 
-      $("#add-interest").on("click", function(event) {
+      	$("#add-interest").on("click", function(event) {
       	event.preventDefault();
       	var interest = $("#interest-input").val().trim();
       	interests.push(interest);
       	renderButtons();
-      });
+      	});
+
       	renderButtons();
 
- });
+
+ });	
+
+		$("button").on("click", function() {
+			var interest = $(this).attr("data-name");
+    		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + interest + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+		$.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+
+        	var results = response.data;
+        	for (var i = 0; i < results.length; i++) {
+        	if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
+
+              // Stores rating
+              var rating = results[i].rating;
+
+              // Creates paragraph tag
+              var p = $("<p>").text("Rating: " + rating);
+
+              // creates an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending paragraph and personImage to the gifDiv
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              // Gif div goes to htmle
+              $("#gifs-appear-here").prepend(gifDiv);
+
+		}
+	}
+});
+
+
+		
 //new section 
 
