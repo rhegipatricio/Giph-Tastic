3238470fb55107e265b7a67606f26a2974276e33
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-04 17:00:15 -0400

    managed comments in giphy.js

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,21 +3,23 @@ $(document).ready(function(){
 var animate = "0"
 var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orlando Magic", "Rick & Morty", "The Goonies"];
 	
+	//function to render the buttons
 	function renderButtons () {
 		$("#interest-view").empty();
 		for (var i = 0; i < interests.length; i++) {
 			var a = $("<button>");
-          // Added class
+    // Adds class of Interest
           	a.addClass("interest");
-          // Adding data attribute to interest
+    // Adding data attribute to interest
          	 a.attr("data-name", interests[i]);
-          // Providing the button's text with a value of the movie at index i
+    // Providing the button's text with a value of the interest at index i
          	 a.text(interests[i]);
-          // Adding the button to the HTML
+    // Adding the button to the HTML
           	$("#interest-view").append(a);
         	}
         }
-
+    // Adds user input interest if entered. If nothing is entered, no blank will be entered into array therefore no
+    // added empty button
       	$("#add-interest").on("click", function(event) {
       	event.preventDefault();
       	var interest = $("#interest-input").val().trim()
@@ -31,53 +33,55 @@ var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orla
 
 
 	
-
+    // function to display images
 		function displayImages (){
-			console.log("click")
+			//console.log("click")
 			var interest = $(this).attr("data-name");
     		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + interest + "&api_key=dc6zaTOxFJmzC&limit=10";
-//to pull from Giphy api
+	//to pull from Giphy api
 		$.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        	console.log(response)
+        	//console.log(response)
 
+    //this will display the results coming from the response data. It will omit rated r and pg-13 images
         	var results = response.data;
         	for (var i = 0; i < results.length; i++) {
         	if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div with the class "item"
+    // Creating a div with the class "item"
               var gifDiv = $("<div class='item'>");
 
-              // Stores rating
+    // Stores rating
               var rating = results[i].rating;
 
-              // Creates paragraph tag
+    // Creates paragraph tag
               var p = $("<p>").text("Rating: " + rating);
 
-              // creates an image tag
+    // creates an image tag
               var personImage = $("<img class = personImage>");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
+    // Giving the image tag an src attribute of a proprty pulled off the
+    // result item
               personImage.attr("src", results[i].images.fixed_height_still.url).val(i);
 
-              // Appending paragraph and personImage to the gifDiv
+    // Appending paragraph and personImage to the gifDiv
               gifDiv.append(p);
               gifDiv.append(personImage);
 
-              // Gif div goes to html
+    // Gif div goes to html
               $("#gifs-appear-here").prepend(gifDiv);
 
 			}
 
 		};
+	//	states that the image will begin still until clicked. Image will then animate
 			$(".personImage").on("click", function(){
-				console.log("click")
-				console.log(this.value)
+				//console.log("click")
+				//console.log(this.value)
 				if (animate == 0){
 				var imageVal = this.value
-				console.log(imageVal)
+				//console.log(imageVal)
 				$(this).attr("src", results[imageVal].images.fixed_height.url)
 				animate ++
 
@@ -86,7 +90,9 @@ var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orla
 				$(this).attr("src", results[imageVal].images.fixed_height_still.url)
 				animate --
 			}
+
 			});
+
 	});
         
 
@@ -94,5 +100,4 @@ var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orla
         $(document).on("click", ".interest", displayImages);
 });
 		
-//new section 
 
