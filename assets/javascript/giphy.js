43dc55dc52054292252b3ca67ef5e34f13107eef
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-04 13:11:43 -0400

    added render button function to giphy.js

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,15 +1,33 @@
-var interests = ["marvel", "dc", "batman", "wolverine", "Michael Jordan"];
+$(document).ready(function(){
+
+var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orlando Magic", "Rick & Morty"];
+	
+	function renderButtons () {
+		$("#interest-view").empty();
+		for (var i = 0; i < interests.length; i++) {
+			var a = $("<button>");
+          // Adding a class
+          	a.addClass("interest");
+          // Adding a data-attribute with a value of the movie at index i
+         	 a.attr("data-name", interests[i]);
+          // Providing the button's text with a value of the movie at index i
+         	 a.text(interests[i]);
+          // Adding the button to the HTML
+          	$("#interest-view").append(a);
+        }
+      }
+
+      $("#add-interest").on("click", function(event) {
+      	event.preventDefault();
+      	var interest = $("#interest-input").val().trim();
+      	interests.push(interest);
+      	renderButtons();
 
 
+      });	
 
+      	renderButtons();
 
-var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-$.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log(response);
     });
 
 
