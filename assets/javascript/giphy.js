2eefe99ffad04f47981806be5ce7f1f4e82b9cf7
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-04 14:16:56 -0400

    fixed errors when buttons disappeared after adding code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -6,28 +6,25 @@ var interests = ["How I Met Your Mother", "Steph Curry", "Michael Jordan", "Orla
 		$("#interest-view").empty();
 		for (var i = 0; i < interests.length; i++) {
 			var a = $("<button>");
-          // Adding a class
+          // Added class
           	a.addClass("interest");
-          // Adding a data-attribute with a value of the movie at index i
+          // Adding data attribute to interest
          	 a.attr("data-name", interests[i]);
           // Providing the button's text with a value of the movie at index i
          	 a.text(interests[i]);
           // Adding the button to the HTML
           	$("#interest-view").append(a);
+        	}
         }
-      }
 
       $("#add-interest").on("click", function(event) {
       	event.preventDefault();
       	var interest = $("#interest-input").val().trim();
       	interests.push(interest);
       	renderButtons();
-
-
-      });	
-
+      });
       	renderButtons();
 
-    });
-
+ });
+//new section 
 
